66a5f6b2a72db6bf0220295341423083f8701598
iazivil <elazirivilasidris1@gmail.com>
2024-03-05 20:04:01 +0100

    segundo cimut: creacion de la clase usuarios

---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+    String nombre;
+    String apellido;
+   int edad;
+
+
+    public Usuario(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
